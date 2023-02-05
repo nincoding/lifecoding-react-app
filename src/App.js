4fa2172bcc3bf1e0ed474404c99bd0c4d498a2ab
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-02-05 18:08:42 +0900

    feat: 리액트 state의 기본 개념과 사용법 실습

--- src/App.js
+++ src/App.js
@@ -59,14 +59,29 @@ class Content extends Component {
 즉, 자바스크립트가 아니다. (jsx문법)
 creat-react-app에서 jsx를 사용하면 알아서 자바스크립트 코드로 컨버팅을 해준다.
 Subject 컴포넌트안에 서로 다른 props를 전달함으로서 항상 같은 값을 출력하던 컴포넌트를 효율성 있게 리팩토링하였다.
+props의 값들이 하드코딩 되있어 불편하다. 따라서 저 값들을 state로 만들고, 그 state의 값을 Subject의 props로 전달하는 것을 통해서 코드를 개선해보자.
+클래스형 컴포넌트에서 state를 만드는 방법: 어떠한 컴포넌트가 실행될 때 render함수보다 먼저 실행이 되면서 그 컴포넌트를 초기화시켜주고 싶은 코드는 constructor안에다가 코드를 작성한다.
+TOC의 부모인 App이 TOC가 가지고 있는 데이터를 사용해서 TOC의 내부 데이터가 바뀔 수 있게끔 만들어보자.
 */
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: {title: 'WEB', sub: "world wide web!" },
+      contents: [
+        {id: 1, title: 'HTML', desc:'HTML is for information'},
+        {id: 2, title: 'CSS', desc: 'CSS is for design'},
+        {id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive'},
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"/>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}/>
         <Subject title="React" sub="For UI"/>
-        <TOC/>
+        <TOC data={this.state.contents}/>
         <Content title='HTML' desc="HTML is HyperText Markup Language."/>
       </div>
     );
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+
+// 외부 App에서 props를 전달받았기 때문에 this.props.data를 가지고 있다.
+// TOC의 데이터가 바뀌었다고, TOC의 로직을 바꾸지 않아도 되는 상태로 만듬
+// 하지만 여러개의 엘리먼트를 자동으로 생성하는 경우에는 콘솔을 켜면 에러가 발생한다.
+// 각각의 리스트 항목들은 key라는 props를 가지고 잇어야 하므로 여러개의 목록을 자동으로 생성할 때에는 
+// key = {} 각각의 목록들을 구분할 수 있는 식별자를 넣어준다.
 class TOC extends Component  {
   render() {
+    let lists = [];
+    const data = this.props.data;
+    for (let i = 0; i < data.length; i++) {
+      lists.push(<li key={data[i].id}><a href={"/content/"+ data[i].id}>{data[i].title}</a></li>)
+    }
+
     return (
       <nav>
         <ul>
-          <li><a href='1.html'>HTML</a></li>
-          <li><a href='2.html'>CSS</a></li>
-          <li><a href='3.html'>JavaScript</a></li>
+          {lists}
         </ul>
       </nav>
     )
