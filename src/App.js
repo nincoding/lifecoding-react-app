7ad0930cb6e01683aad09a8199d7655e988f84d2
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-02-08 21:25:48 +0900

    feat: 클래스형 리액트 컴포넌트 이벤트 만들기 실습

--- src/App.js
+++ src/App.js
@@ -70,6 +70,7 @@ class App extends Component {
     super(props);
     this.state = {
       mode: 'welcome',
+      selected_content_id: 2,
       subject: {title: 'WEB', sub: "world wide web!" },
       welcome: {title: 'Welcome', desc:'Hello, React!'},
       contents: [
@@ -90,14 +91,30 @@ class App extends Component {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     } else if (this.state.mode === 'read') {
+      var i = 0;
+      while (i < this.state.contents.length) {
+        var data = this.state.contents[i];
+        if (data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i++;
+      }
+      /*
       _title = this.state.contents[0].title;
       _desc = this.state.contents[0].desc;
+      */
     }
     return (
       <div className="App">
-        {/*<Subject title={this.state.subject.title} sub={this.state.subject.sub}/>
-        <Subject title="React" sub="For UI"/>*/}
-      <header>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}
+        onChangePage = {function () {
+          this.setState({mode: 'welcome'});
+        }.bind(this)}
+        />
+        <Subject title="React" sub="For UI"/>
+      {/*<header>
         <h1>
           <a href='/' onClick={function (e) {
             e.preventDefault();
@@ -115,9 +132,19 @@ class App extends Component {
           </a>
         </h1>
         {this.state.subject.sub}
-      </header>
+        </header>*/}
         
-        <TOC data={this.state.contents}/>
+        <TOC 
+        onChangePage= {
+          function(id) {
+            this.setState({
+          mode: 'read',
+          selected_content_id: Number(id),
+          });
+
+          }.bind(this)
+        }
+        data={this.state.contents}/>
         <Content title={_title} desc={_desc}/>
       </div>
     );
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -8,9 +8,31 @@ import React, { Component } from 'react';
 class TOC extends Component  {
   render() {
     let lists = [];
+    /*
     const data = this.props.data;
     for (let i = 0; i < data.length; i++) {
       lists.push(<li key={data[i].id}><a href={"/content/"+ data[i].id}>{data[i].title}</a></li>)
+    }
+    */
+
+    var data = this.props.data;
+    var i = 0;
+    while (i < data.length) {
+      lists.push(
+        <li key={data[i].id}>
+          <a
+          href={'/content/' + data[i].id}
+          data-id={data[i].id}
+          onClick={function(e) {
+            
+            e.preventDefault();
+            this.props.onChangePage(e.target.dataset.id);
+          }.bind(this)}
+          >{data[i].title}
+          </a>
+        </li>
+      )
+      i++;
     }
 
     return (
